555df474b85ad3fefb5835c6d700ae88b2defab8
Caleb Thrash <calebaileythrash@gmail.com>
2023-11-04 13:16:14 -0500

    Created hash table for stroing calorieLog.

--- calorieLog.java
+++ calorieLog.java
@@ -1,3 +1,25 @@
-public class calorieLog {
+import java.io.*;
+import java.util.*;
 
-}
+public class calorieLog {
+    public static void main(String args[])
+        {
+            // No need to mention the
+            // Generic type twice
+            Hashtable<String, Integer> calLog = new Hashtable<>();
+
+
+            // Inserting the Elements
+            calLog.put("oct2", 1000);
+            calLog.put("nov1", 1570);
+            calLog.put("nov3", 2050);
+
+
+
+            // Print mappings to the console
+            System.out.println("Mappings of ht1 : " + calLog );
+
+        }
+    }
+
+
